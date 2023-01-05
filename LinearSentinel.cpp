316090da3bcbf49ad87fb881104e44d04e1c8ec9
omkar-khalkar <115702207+omkar-khalkar@users.noreply.github.com>
2023-01-05 23:25:43 +0530

    LinearSentinel.cpp

--- LinearSentinel.cpp
+++ LinearSentinel.cpp
@@ -1,5 +1,5 @@
 /*
-Write a pythonprogram to store roll numbers of student in array who attended
+Write a python program to store roll numbers of student in array who attended
 training program in random order. Write function for searching whether particular
 student attended training program or not, using Linear search and Sentinel search.
 */
