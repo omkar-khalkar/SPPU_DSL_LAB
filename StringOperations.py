d945e3d4b5ab19d7b838ce19f3e1ec583f23507b
omkar-khalkar <115702207+omkar-khalkar@users.noreply.github.com>
2023-01-05 23:31:45 +0530

    StringOperations.py

--- StringOperations.py
+++ StringOperations.py
@@ -1,5 +1,5 @@
 
-/*
+"""
 
 Write a Python program to compute following operations on String:
 a) To display word with the longest length
@@ -7,7 +7,8 @@ b) To determines the frequency of occurrence of particular character in the stri
 c) To check whether given string is palindrome or not
 d) To display index of first appearance of the substring
 e) To count the occurrences of each word in a given string
-*/
+
+"""
 def longest_word():
 
     str = input("Enter the string :")
